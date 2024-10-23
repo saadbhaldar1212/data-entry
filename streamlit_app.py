46adf8bb30928c2cf8d865fbbab39f458c7b1f28
Mohammad Saad Bhaldar <saadbhaldar1212@gmail.com>
2024-10-23 18:24:29 +0530

    Changes applied - 23-Oct-2024

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
+import uuid
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+column = ["Name","Age/Gender","Complaint","Phone Number","Date","Time","Follow_Up"]
+
+if 'data' not in st.session_state:
+    st.session_state.data = []
+
+st.write('Data Entry')
+form = st.form('data_entry')
+value = form.text_area(label='Enter value: ')
+button = form.form_submit_button(label='Submit')
+if button:
+    result = value.splitlines()
+    st.session_state.data.append(result)
+
+df = pd.DataFrame(st.session_state.data, columns=column)
+if not df.empty:
+    st.write('Data output')
+    st.dataframe(df)
+    save_form = st.form('save_form')
+    file_name = save_form.text_input('File name')
+    save_to_csv_button = save_form.form_submit_button(label='Save to CSV')
+    if save_to_csv_button:
+        df.to_csv(f'{file_name}.csv', index=False)
+        st.write('File Saved successfully')
