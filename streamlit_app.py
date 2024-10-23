61b8b55e43d5a49af751dc5faba0dc1281547305
Mohammad Saad Bhaldar <saadbhaldar1212@gmail.com>
2024-10-23 18:53:50 +0530

    Changes applied III - 23-Oct-2024

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,19 +8,17 @@ if 'data' not in st.session_state:
 
 st.write('Data Entry')
 form = st.form('data_entry')
-value = form.text_area(label='Enter value: ')
+value = form.text_area(label='Enter value: ', height=300)
 button = form.form_submit_button(label='Submit')
 if button:
     result = value.splitlines()
     st.session_state.data.append(result)
 
 df = pd.DataFrame(st.session_state.data, columns=column)
+# df = st.dataframe(data=st.session_state.data)
 if not df.empty:
     st.write('Data output')
     st.dataframe(df)
-    save_form = st.form('save_form')
-    file_name = save_form.text_input('File name')
-    save_to_csv_button = save_form.form_submit_button(label='Save to CSV')
-    if save_to_csv_button:
-        df.to_csv(f'{file_name}.csv', index=False)
-        st.write('File Saved successfully')
+    # # save_form = st.form('save_form')
+    # file_name = st.text_input('File name')
+    # st.download_button(file_name=f'{file_name}.csv', label='Download', data=df)
